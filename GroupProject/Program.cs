382df2f449e3960b07d1dee9f7a8ae2de4a353e2
wpachec2 <fredojpacheco@gmail.com>
2023-01-25 16:27:07 -0500

    Added list of names and ages

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using GroupProject;
+
+List<Person> people = new List<Person>()
+{
+    new Person("Wilfredo Pacheco", 26),
+    new Person("Belle Baxley", 31),
+    new Person("Tee Sotomi", 40)
+};
+
